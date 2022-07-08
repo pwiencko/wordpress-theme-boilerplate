c883c1ccd88d837291c118e7f646df42e9ed3053
Przemysław Wiencko <przemyslaw@wiencko.pl>
2022-07-08 21:55:00 +0200

    feature - webpack & css optimization

--- includes/scripts.php
+++ includes/scripts.php
@@ -2,7 +2,7 @@
 
 function wp_boilerplate_theme_scripts() {
     $theme_version = wp_get_theme()->get('Version');
-    wp_enqueue_script( 'theme-js', get_template_directory_uri() . '/assets/js/theme.js', array(), $theme_version, true );
+    wp_enqueue_script( 'theme-js', get_template_directory_uri() . '/assets/js/theme.bundle.js', array(), $theme_version, true );
 
 }
 add_action( 'wp_enqueue_scripts', 'wp_boilerplate_theme_scripts' );
